6d71c5fe28050211022a0b3c3363f14f47686db9
Pranav Kumar <pranav.jenus@gmail.com>
2024-01-19 00:28:06 +0530

    Update and rename dijointSetUsingRanking.cpp to dijointSetUsingRankingOrSize.cpp

--- dijointSetUsingRankingOrSize.cpp
+++ dijointSetUsingRankingOrSize.cpp
@@ -7,12 +7,14 @@ class Disjoint
 {
     vector<int>parent;
     vector<int>ranking;
+    vector<int>size_;
     int N;
 public:
     Disjoint(int n)
     {
         N = n;
         ranking.resize(N,0);
+        size_.resize(N,1);
         parent.resize(N);
         makeSet();
         print_Obj();
@@ -33,10 +35,9 @@ public:
             int res = findParent(parent[node]);
             parent[node] = res;
         }
-        //return parent[node];
     }
 
-    void union_(int u,int v)
+    void unionbyRank(int u,int v)
     {
         int root_u = findParent(u);
         int root_v = findParent(v);
@@ -55,9 +56,31 @@ public:
         else
         {
             parent[root_u] = root_v;
-            ranking[root_v] = ranking[root_v]+1;
+            ranking[root_v]++;
         }
     }
+
+    void unionbySize(int u,int v)
+    {
+        int root_u = findParent(u);
+        int root_v = findParent(v);
+
+        if(root_u == root_v)
+            return;
+
+        if(size_[u]<size_[v])
+        {
+            parent[root_u] = root_v;
+            size_[root_v] += size_[root_u];
+        }
+        else
+        {
+            parent[root_v] = root_u;
+            size_[root_u] += size_[root_v];
+        }
+
+    }
+
     void print_Obj()
     {
         cout << "__Parent Array__" << endl;
@@ -69,25 +92,54 @@ public:
         for(int i=0;i<N;i++)
             cout << ranking[i] << "\t";
         cout << endl;
+
+        cout << "__Size_Array__" << endl;
+        for(int i=0;i<N;i++)
+            cout << size_[i] << "\t";
+        cout << endl;
     }
 };
 
 int main()
 {
+    
     Disjoint d(5);
-    d.union_(0,1);
+    d.unionbyRank(0,1);
     d.print_Obj();
-    d.union_(2,3);
+    d.unionbyRank(2,3);
     d.print_Obj();
-    d.union_(4,3);
+    d.unionbyRank(4,3);
     d.print_Obj();
 
     //d.print_Obj();
     if(d.findParent(4)==d.findParent(4))
     {
-        cout << "Belongs to Same Set" << endl;
+        cout << "Belongs to Same Set Using Ranking " << endl;
     }
     else
-        cout << "Doesn't belong to Same Set" << endl;
+        cout << "Doesn't belong to Same Set Using Ranking " << endl;
+
+    
+    Disjoint d2(7);
+    d2.unionbySize(1,2);
+    d2.print_Obj();
+    d2.unionbySize(2,3);
+    d2.print_Obj();
+    d2.unionbySize(4,5);
+    d2.print_Obj();
+    d2.unionbySize(6,7);
+    d2.print_Obj();
+    d2.unionbySize(5,6);
+    d2.print_Obj();
+    //d2.unionbySize(3,7);
+    d2.print_Obj();
+
+    //d.print_Obj();
+    if(d2.findParent(2)==d2.findParent(6))
+    {
+        cout << "Belongs to Same Set Using Size" << endl;
+    }
+    else
+        cout << "Doesn't belong to Same Set Using Size" << endl;
     return 0;
 }
