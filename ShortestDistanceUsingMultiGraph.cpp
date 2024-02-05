5bb7667fb8d3e3f869670546a156c20ff1b77d19
Pranav Kumar <pranav.jenus@gmail.com>
2024-02-06 00:04:19 +0530

    Update ShortestDistanceUsingMultiGraph.cpp
    Added Code for Path Generation

--- ShortestDistanceUsingMultiGraph.cpp
+++ ShortestDistanceUsingMultiGraph.cpp
@@ -1,28 +1,53 @@
 #include <iostream>
 #include <climits>
+#include <vector>
 
 #define INF INT_MAX
 #define N 8
 
 using namespace std;
 
-int shortestPathDistance(int graph[N][N])
+int shortestPathDistance(int graph[N][N],vector<int>& path)
 {
     int dist[N];
     dist[N-1]=0;
+    int cheapestCostNode[N];
 
+    cheapestCostNode[N-1]=N-1;
     for(int i=N-2;i>=0;i--)
     {
         dist[i]=INF;
+        cheapestCostNode[i] = i;
 
+        int min_j=i;
         for(int j=i;j<N;j++)
         {
-            if(graph[i][j]!=INF)
+            if(graph[i][j]!=INF && dist[i]>graph[i][j]+dist[j])
             {
-                dist[i] = min(dist[i],graph[i][j]+dist[j]);
+                dist[i] = graph[i][j]+dist[j];
+                min_j=j;
             }
         }
+        cheapestCostNode[i] = min_j;
     }
+    cout << "cheapestCostNode Generated:\t";
+    for(int i=0;i<N;i++)
+    {
+        cout << cheapestCostNode[i] << "\t";
+    }
+
+    cout << endl;
+    int node = cheapestCostNode[0];
+    path.push_back(0); // Start Node is 0
+    path.push_back(node); // Immediate Node is from cheapestCostNode[0]
+    do
+    {
+        node = cheapestCostNode[node];
+        path.push_back(node);
+
+    }while(node!=N-1);
+
+
     return dist[0];
 }
 
@@ -39,7 +64,14 @@ int main()
       {INF, INF, INF, INF, INF, INF, INF, INF}};
 
 
-    int res = shortestPathDistance(graph);
-    cout << "Shortest Distance=" << res << endl;
+    vector<int> path;
+    int res = shortestPathDistance(graph,path);
+    cout << "Shortest Distance  =     " << res << endl;
+
+    cout << "Shortest Path :\t";
+    for(auto ss:path)
+        cout << ss << "\t";
+
+    cout << endl;
     return 0;
 }
